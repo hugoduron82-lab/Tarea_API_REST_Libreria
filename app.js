b9e88e9e7fce4d1880739d786a22e5e1dda6b232
hugoduron82-lab <hugoduron82@gmail.com>
2026-02-08 13:10:54 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express;
+const PORT = 3000;
+
+app.use(express.json());
+
+let libros = [
+                {
+                    id: 1,
+                    titulo: 'El Silmarillion',
+                    autor: 'J.R.R. Tolkien',
+                    genero: 'Fantasía épica',
+                    anioPublicacion: 1977
+                },
+                {
+                    id: 2,
+                    titulo: 'Cartas del Diablo a su Sobrino',
+                    autor: 'C.S. Lewis',
+                    genero: 'Ficción teológica',
+                    anioPublicacion: 1942
+                },
+                {
+                    id: 3,
+                    titulo: '"Narraciones extraordinarias',
+                    autor: 'Edgar Allan Poe',
+                    genero: 'Terror gótico',
+                    anioPublicacion: 1856
+                },
+                {
+                    id: 4,
+                    titulo: 'El SilmarLa Torre Oscura I: El pistolero',
+                    autor: 'Stephen King',
+                    genero: 'Fantasía oscura',
+                    anioPublicacion: 1982
+                },
+                {
+                    id: 5,
+                    titulo: 'El Contacto',
+                    autor: 'J.R.Carl Sagan. Tolkien',
+                    genero: 'Ciencia ficción',
+                    anioPublicacion: 1985
+                }
+            ]
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`El servidor esta escuchando en http://localhost:${PORT}`);
+});
