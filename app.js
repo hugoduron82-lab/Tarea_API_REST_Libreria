85b57e2fe3f395ce7f31367aab3ebcf19b0602a1
hugoduron82-lab <hugoduron82@gmail.com>
2026-02-08 13:28:01 -0600

    creacion del Get para llamar a todos los libros guardados en el JSON

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const app = express;
+const { message } = require('statuses');
+const app = express();
 const PORT = 3000;
 
 app.use(express.json());
@@ -43,7 +44,9 @@ let libros = [
             ]
 
 
-
+app.get('/libros',(req,res)=>{
+    res.status(200).json({status:200,message:'Success',data: libros});
+})
 
 app.listen(PORT, ()=>{
     console.log(`El servidor esta escuchando en http://localhost:${PORT}`);
